ca340e20d1e3530c193f1955e424f38d0c805f8d
yoonsujinn <yoonsujin2@naver.com>
2026-02-20 16:35:35 +0900

    1111

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter> 
-  {/* BrowserRouter는 현재 주소를 저장하고 감지함. */}
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {HashRouter} from "react-router-dom";
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    {/* BrowserRouter는 현재 주소를 저장하고 감지함. */}
     <App />
-  </BrowserRouter>,
-)
+  </HashRouter>,
+);
